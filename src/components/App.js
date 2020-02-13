f2182e4dee5ea127837cdf577e6d926afc6f5613
Paulino Jr <paulino.junior@outlook.com>
2020-02-12 23:06:16 -0300

    Adding details view

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList';
-import './styles/App.css'
+import DetailView from './DetailView';
+import './styles/App.css';
 
-export default class App extends Component { 
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
     this.state = {};
   }
 
   render() {
-    return(
+    return (
       <div className="App">
-        <PokeList></PokeList>
+        <PokeList />
+        <DetailView />
       </div>
     );
   }
 }
+
+export default App;
